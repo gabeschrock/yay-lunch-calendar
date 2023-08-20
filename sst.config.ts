09c0b1fa5caa02b9b5454f67db431bda1f981304
Eric Schrock <eschrock@gmail.com>
2023-08-20 19:37:12 -0400

    ran create-sst

--- sst.config.ts
+++ sst.config.ts
@@ -1,11 +1,20 @@
-import type { SSTConfig } from "sst";
+import { SSTConfig } from "sst";
+import { NextjsSite } from "sst/constructs";
 
 export default {
-  config(input) {
+  config(_input) {
     return {
       name: "yay-lunch-calendar",
       region: "us-east-1",
     };
   },
-  stacks(app) {},
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new NextjsSite(stack, "site");
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
 } satisfies SSTConfig;
