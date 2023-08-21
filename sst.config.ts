2c986d8465646e4be7864c8bf364b1b0d3893cc7
Gabe Schrock <gabeschrock@gmail.com>
2023-08-20 20:16:16 -0400

    configure domain name

--- sst.config.ts
+++ sst.config.ts
@@ -10,7 +10,12 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site", {
+        customDomain: {
+          domainName: stack.stage == "prod" ? "yay.schrock.io" : `yay-${stack.stage}.schrock.io`,
+          hostedZone: "schrock.io"
+        }
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
